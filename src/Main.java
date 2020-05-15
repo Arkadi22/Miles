f3796114b18c8ab81de42a9c329cb39dc4d5e1d5
Arkadi <arkadi.hakobyan22@gmail.com>
2020-05-15 12:12:13 +0400

    Change java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         double priceticketrub = 9000.20D;
         int priceInKops = (int) (priceticketrub * 100);
-        double onemilecostrub = 20D;
-        int onemilecostKops = (int) (onemilecostrub * 100);
-        int milesamount = priceInKops / onemilecostKops;
+        double oneMileCostrub = 20D;
+        int oneMileCostKops = (int) (oneMileCostrub * 100);
+        int milesamount = priceInKops / oneMileCostKops;
         System.out.println(milesamount);
     }
 }
